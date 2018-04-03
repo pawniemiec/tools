1e50cee1fdf1bd80a3340c395ab2410f86f1d9bb
Pawel Niemiec <9714176+pawniemiec@users.noreply.github.com>
2018-04-03 17:53:36 +0100

    Automatic push at 20180403-175336

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tools
 Collection of useful scripts
+
+
+This repository is automatically updated every 4h by `git_push.sh` script
--- git_push.sh
+++ git_push.sh
@@ -3,6 +3,7 @@
 DIR=$(dirname "$0")
 echo "Starting in ${DIR}"
 
+cd ${DIR}
 git add .
 git commit -m "Automatic push at $(date +%Y%m%d-%H%M%S)"
 git secrets --install -f && git secrets --scan && git push origin master
